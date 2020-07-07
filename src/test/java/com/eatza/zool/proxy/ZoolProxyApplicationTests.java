67b314c5a1e68eaccbb5e2982e8ce6c086775b37
Aathira HN <M1053645@mindtree.com>
2020-07-07 20:26:42 +0530

    config-server-sommit

---
+++ src/test/java/com/eatza/zool/proxy/ZoolProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatza.zool.proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoolProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
